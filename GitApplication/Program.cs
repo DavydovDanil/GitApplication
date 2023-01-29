29970bbc723cb40310f3d78d3d6c453943d5f918
ТАТЫЭНА <tshatkov4@gmail.com>
2023-01-29 19:03:56 +0300

    fsdfs

--- GitApplication/Program.cs
+++ GitApplication/Program.cs
@@ -6,6 +6,28 @@
         {
 
         }
+        public void MatrixA(int n, int m)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            int[,] mass = new int[n, m];
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m - 1; ++j)
+                {
+                    if (i == j)
+                        mass[i, j + 1] = i + j;
+                }
+            }
+
+            for (int i = 0; i < n; ++i, Console.WriteLine())
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    Console.Write(mass[i, j] + " ");
+                }
+            }
+        }
         public void MatrixB(int n, int m)
         {
             Console.ForegroundColor = ConsoleColor.Magenta;
