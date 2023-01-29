cf157079f040f32fa18cfcedd5d7921d8f7d7bdb
ТАТЫЭНА <tshatkov4@gmail.com>
2023-01-29 19:58:22 +0300

    ВСЁ

--- GitApplication/Program.cs
+++ GitApplication/Program.cs
@@ -28,9 +28,37 @@
                 }
             }
         }
+        public void MatrixB(int n, int m)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            int[,] mass = new int[n, m];
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m - 1; ++j)
+                {
+                    if (i == j)
+                        mass[i, j + 1] = i + j;
+                }
+            }
+
+            for (int i = 0; i < n; ++i, Console.WriteLine())
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    Console.Write(mass[i, j] + " ");
+                }
+            }
+        }
         public void PrintMyName()
         {
             Console.WriteLine("Shatkov Timofey Konstantinovich");
+            Console.WriteLine("10.4");
+            Console.WriteLine("July, 7");
+        }
+        public void PrintMyName1()
+        {
+            Console.WriteLine("Davydov Danil Dmitrievich");
             Console.WriteLine("10.4");
             Console.WriteLine("July, 7");
         }
